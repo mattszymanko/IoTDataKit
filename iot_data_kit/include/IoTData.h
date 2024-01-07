d4a1fd0832d0de29e72324eaef2d3419ad2dfa03
Matthias Szymanko <62217720+mattszymanko@users.noreply.github.com>
2024-01-07 11:44:07 +0100

    Initial file created

---
+++ iot_data_kit/include/IoTData.h
@@ -0,0 +1,39 @@
+// IoTData.h
+#ifndef IOT_DATA_H
+#define IOT_DATA_H
+
+#include <vector>
+
+class IoTData {
+private:
+    std::vector<double> data;
+
+public:
+    // Constructor
+    IoTData(const std::vector<double>& initialData);
+
+    // Basic data manipulation functions
+    void appendData(double newData);
+    void clearData();
+    size_t getDataSize() const;
+
+    // Data filtering functions
+    void filterOutliers(double threshold);
+
+    // Statistical analysis functions
+    double calculateMean() const;
+    double calculateStandardDeviation() const;
+
+    // Data transformation functions
+    void scaleData(double scaleFactor);
+    void normalizeData();
+
+    // Data export/import functions
+    void exportDataToFile(const std::string& filename) const;
+    void importDataFromFile(const std::string& filename);
+
+    // Data visualization functions
+    void plotData() const;
+};
+
+#endif // IOT_DATA_H
